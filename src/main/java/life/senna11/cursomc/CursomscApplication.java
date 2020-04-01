c6a79326fc241826e4bc1d05c05925514757eb7b
Diego dos Santos <diego.dos.santos@philips.com>
2020-04-01 13:00:41 -0300

    Criando operação de instanciação

--- src/main/java/life/senna11/cursomc/CursomscApplication.java
+++ src/main/java/life/senna11/cursomc/CursomscApplication.java
@@ -1,13 +1,33 @@
 package life.senna11.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import life.senna11.cursomc.domain.Categoria;
+import life.senna11.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomscApplication {
+public class CursomscApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomscApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
