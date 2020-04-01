eefe8e43cbead3c594fcc72cedb3175a3f553b5c
Diego dos Santos <diego.dos.santos@philips.com>
2020-04-01 13:24:19 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/life/senna11/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package life.senna11.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import life.senna11.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
