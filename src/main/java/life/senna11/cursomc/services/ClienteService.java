21f3d67d3880629d6ffdd43c95de2fbb6c473764
Diego dos Santos <diego.dos.santos@philips.com>
2020-04-03 08:37:39 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/life/senna11/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package life.senna11.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import life.senna11.cursomc.domain.Cliente;
+import life.senna11.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente busca(Integer id){
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new life.senna11.cursomc.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id +
+				", Tipo: " + Cliente.class.getName()));
+	}
+
+}
