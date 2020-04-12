fb7715d498c807baca6fa8f02749c8dcd8c9615a
Diego dos Santos <diego.dos.santos@philips.com>
2020-04-12 10:40:10 -0300

    Validação sintática com Bean Validation

--- src/main/java/life/senna11/cursomc/services/CategoriaService.java
+++ src/main/java/life/senna11/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import life.senna11.cursomc.domain.Categoria;
+import life.senna11.cursomc.dto.CategoriaDTO;
 import life.senna11.cursomc.repositories.CategoriaRepository;
 import life.senna11.cursomc.services.exceptions.DataIntegrityException;
 
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linePerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
