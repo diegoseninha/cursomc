9c077ddde2de4b14a44453c32a923aec4236dec7
Diego dos Santos <diego.dos.santos@philips.com>
2020-04-21 11:00:12 -0300

    Validação customizada de CPF ou CNPJ na inserção de cliente

---
+++ src/main/java/life/senna11/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,40 @@
+package life.senna11.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import life.senna11.cursomc.domain.enums.TipoCliente;
+import life.senna11.cursomc.dto.ClienteNewDTO;
+import life.senna11.cursomc.resources.exceptions.FieldMessage;
+import life.senna11.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+		
+	}
+	
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		for (FieldMessage e: list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
