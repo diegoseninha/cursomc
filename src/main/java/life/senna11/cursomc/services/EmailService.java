9e72ff03d22c92589d68615ab1caab96605987fe
Diego dos Santos <diego.dos.santos@philips.com>
2020-05-09 11:29:42 -0300

    Email HTML

--- src/main/java/life/senna11/cursomc/services/EmailService.java
+++ src/main/java/life/senna11/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package life.senna11.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import life.senna11.cursomc.domain.Pedido;
@@ -9,5 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 
 }
--- src/main/java/life/senna11/cursomc/services/MockEmailService.java
+++ src/main/java/life/senna11/cursomc/services/MockEmailService.java
@@ -1,8 +1,12 @@
 package life.senna11.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
+
+import life.senna11.cursomc.domain.Pedido;
 
 public class MockEmailService extends AbstractEmailService{
 	
@@ -14,6 +18,11 @@ public class MockEmailService extends AbstractEmailService{
 		LOG.info(msg.toString());
 		LOG.info("Email enviado!");
 	}
-	
 
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML... ");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado!");	
+	}
 }
